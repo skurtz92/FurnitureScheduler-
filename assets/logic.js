7c0e54b3ebbe9b78a8d15700cf445cd55aa3d596
Marlo Bennett <marlomariebennett@gmail.com>
2016-09-08 20:41:58 -0500

    separated html, css, and js and added some links

--- assets/logic.js
+++ assets/logic.js
@@ -1,19 +1,17 @@
 $(document).ready(function() {
 
-    // page is now ready, initialize the calendar...
-
-    $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    })
-
-});
-
-$('#calendar').fullCalendar({
-    weekends: false // will hide Saturdays and Sundays
-});
-$('#calendar').fullCalendar({
-    dayClick: function() {
-        alert('a day has been clicked!');
-    }
-});
-$('#calendar').fullCalendar('next');
+		$('#calendar').fullCalendar({
+			defaultDate: '2016-09-12',
+			editable: true,
+			eventLimit: true, // allow "more" link when too many events
+			events: [
+				{
+					title: 'All Day Event',
+					start: '2016-09-01'
+				},
+			
+				
+			]
+		});
+		
+	});
