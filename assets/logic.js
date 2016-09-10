bd6092fd6c1bffb3028d11f3c97009ee7901fabc
Marlo Bennett <marlomariebennett@gmail.com>
2016-09-09 23:37:49 -0500

    added notes to js

--- assets/logic.js
+++ assets/logic.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
 		$('#calendar').fullCalendar({
 			defaultDate: '2016-09-12',
 			editable: true,
-			eventLimit: true, // allow "more" link when too many events
+			//this will show only Monday-Friday
+			//weekends: false,
+			eventLimit: true, // limits events to five a day
 			events: [
 				{
 					title: 'All Day Event',
