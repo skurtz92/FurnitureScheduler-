ccc5721fc6f9e27d914b40e566f73c3860c3963c
Marlo Bennett <marlomariebennett@gmail.com>
2016-09-10 12:54:51 -0500

    updated js

--- assets/logic.js
+++ assets/logic.js
@@ -1,19 +1,56 @@
+var deliveryDate;
+var companyName;
+var deliveryAddress;
+var itemNumber;
+var timeRequired;
+var specialInstructions;
+var enteredBy;
+
 $(document).ready(function() {
 
-		$('#calendar').fullCalendar({
-			defaultDate: '2016-09-12',
-			editable: true,
-			//this will show only Monday-Friday
-			weekends: false,
-			eventLimit: true, // limits events to five a day
-			events: [
-				{
-					title: 'All Day Event',
-					start: '2016-09-01'
-				},
+
+
+	$('#calendar').fullCalendar({
+		defaultDate: '2016-09-12',
+		editable: true,
+		//this will show only Monday-Friday
+		weekends: false,
+		eventLimit: true, // limits events to five a day
+		events: [
+			{
+				title: 'All Day Event',
+				start: '2016-09-01'
+			},
 			
 				
-			]
-		});
-		
+		]
+	//end of full calendar function
 	});
+
+	//$('#datepicker').datepicker();
+
+	/*$('#datepicker').on('change', function(event){
+    deliveryDate = event.currentTarget.value;
+	});*/
+
+	//get user inputs
+	deliveryDate = $("#delDate").val();
+	companyName = $("#compName").val().trim();
+	deliveryAddress = $("#delAddress").val().trim();
+	itemNumber = $("#itemNum").val().trim();
+	timeRequired = $("#projectedHours").val().trim();
+	specialInstructions = $("#specInstr").val().trim();
+	enteredBy = $("#enterBy").val().trim();
+
+	var newDeliveryListing = {
+		title: companyName,
+		start: deliveryDate,
+		addy: deliveryAddress,
+		items: itemNumber,
+		time: timeRequired,
+		instrux: specialInstructions,
+		salesperson: enteredBy
+	}
+
+//end of document ready function		
+});
