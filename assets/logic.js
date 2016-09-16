718599e42a3e5287e5fe593052aa09b3a2ba6ee0
Jose Martinez <laderhoser@gmail.com>
2016-09-15 20:57:13 -0500

    starter location set to Austin

--- assets/logic.js
+++ assets/logic.js
@@ -152,7 +152,7 @@ $(document).ready(function() {
 function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 15,
-          center: {lat: -34.397, lng: 150.644}
+          center: {lat: 30.374784, lng: -97.676455}
         });
         var geocoder = new google.maps.Geocoder();
 
