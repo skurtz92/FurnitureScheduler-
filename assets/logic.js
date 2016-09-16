dde4cd75b20ecf2148018815371a20e5978ffdd3
Jose Martinez <laderhoser@gmail.com>
2016-09-15 20:39:48 -0500

    map working

--- assets/logic.js
+++ assets/logic.js
@@ -150,14 +150,29 @@ $(document).ready(function() {
 });
 
 function initMap() {
-    var mapDiv = document.getElementById('map');
-        var map = new google.maps.Map(mapDiv, {
-            center: {
-                lat: 44.540,
-                lng: -78.546
-        },
-        zoom: 8
-    });
-}
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: {lat: -34.397, lng: 150.644}
+        });
+        var geocoder = new google.maps.Geocoder();
 
+        document.getElementById('submit').addEventListener('click', function() {
+          geocodeAddress(geocoder, map);
+        });
+      }
+
+      function geocodeAddress(geocoder, resultsMap) {
+        var address = document.getElementById('delAddress').value;
+        geocoder.geocode({'address': address}, function(results, status) {
+          if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+              map: resultsMap,
+              position: results[0].geometry.location
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });
+      }
 initMap();
